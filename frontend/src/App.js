297b6f0a9ea0f6fcde9b20f8a4df06fcbb088d01
Garett Bare <Garett.Bare@yahoo.com>
2022-04-20 20:23:07 -0500

    Testing more branches

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,6 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import NotFound from "./pages/NotFound/NotFound";
-
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
@@ -30,7 +28,6 @@ function App() {
         />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<NotFound/>}/>
       </Routes>
       <Footer />
     </div>
