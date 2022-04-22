f749276707120133a112e903364a2c717fb5943a
Dustin Barker <barkerd85@yahoo.com>
2022-04-22 14:32:05 -0600

    About me route complete

--- backend/models/aboutMe.js
+++ backend/models/aboutMe.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const aboutSchema = new mongoose.Schema({
+    text: {type:String, minLength: 2, maxLength: 1028, default:"About Me..."},
+
+});
+
+const About = mongoose.model("About", aboutSchema);
 
 
-const aboutMeSchema = mongoose.Schema({
-    about: { type: String, required: true, minLength: 50, maxLength: 1000 },
-    _id: { type: String, required: true,}
-  })
-
-module.exports.aboutMeSchema = aboutMeSchema;
+function validateAbout(about){
+    const schema = Joi.object({
+        text: Joi.string().min(2).max(1028),
+    });
+    return schema.validate(about);
+    };
+    module.exports = {
+        aboutSchema, 
+        About,
+        validateAbout
+    }
--- backend/routes/aboutMe.js
+++ backend/routes/aboutMe.js
@@ -1,24 +1,37 @@
 const { User, validateLogin, validateUser } = require("../models/user");
+const {About, validateAbout} = require("../models/about")
 
-const auth = require("../middleware/auth");
 
 const express = require("express");
-const { aboutMeSchema } = require("../models/aboutMe");
 const router = express.Router();
 
-
-
-// POST about me to Authorized user
-router.post("/about/:userId", [ auth ], async (req, res) => {
+router.post("/:userId", async (req, res) => {
     try {
-      const user = await User.findById(req.params.userId);
-      if (!user)
+        let {error} = validateAbout(req.body);
+        if (error) return res.status(400).send(`Your About me status had the following errors: ${error}`)
+
+        const user = await User.findById(req.params.userId);
+        if (!user)
         return res
-          .status(400)
-          .send(`User with id ${req.params.userId} does not exist!`);
-      await aboutMeSchema.update();
-      return res.send(user);
+        .status(400)
+        .send(`User with id ${req.params.userId} does not exist!`);
+
+        const newAboutMe = await About(req.body);
+        user.about = newAboutMe
+
+        await user.save()
+            return res.status(201).send(user);
+        
     } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
+        return res.status(500).send(`Internal Server Error: ${ex}`);
     }
-  });
+});
+
+
+module.exports = router;
+      
+        
+          
+          
+    
+   
