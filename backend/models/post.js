6d9b513caa85e09fe266f52fd1dd14b3324fc52e
varNum462 <100005058+varNum462@users.noreply.github.com>
2022-04-21 21:35:00 -0400

    Commit to merge for testing

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const jwt = require("jsonwebtoken");
-const { number } = require("joi");
-const { User } = require("../models/user");
+
+const replySchema = new mongoose.Schema({
+    message: {type:String, minlength: 2, maxlength:1028},
+    dateAdded: {type:Date, default: Date.now()},    
+  }); 
+
+const commentSchema = new mongoose.Schema({
+  message: {type:String, minlength: 2, maxlength:1028},
+  replies: [{type:replySchema}],
+  dateAdded: {type:Date, default: Date.now()},   
+});
 
 const postSchema = mongoose.Schema({
   message: { type: String, required: true, minLength:5, maxlength:1028 },
@@ -16,23 +24,10 @@ const postSchema = mongoose.Schema({
   dislikes: {type: Number,default: 0}, 
   dateAdded: {type:Date, default: Date.now()},
   comments: [{type:commentSchema}], 
+  friends: [],
+  pendFriends: [],
 });
 
-const commentSchema = new mongoose.Schema({
-  message: {type:String, required: true, minlength: 2, maxlength:1028},
-  postId: {type:String, required: true},
-  replies: [{type:replySchema}],
-  dateAdded: {type:Date, default: Date.now()}, 
-  likes: {type:Number,default:0},
-  dateAdded: {type:Date, default: Date.now()},
-  dislikes: {type:Number,default:0},  
-});
-
-const replySchema = new mongoose.Schema({
-  message: {type:String, required: true, minlength: 2, maxlength:1028},
-  image: {type:String},
-  dateAdded: {type:Date, default: Date.now()},    
-}); 
 
 function validatePost(post){
   const schema = Joi.object({
@@ -44,10 +39,17 @@ function validatePost(post){
 
 function validateComment(comment){
   const schema = Joi.object({
-      message: Joi.string().min(2).max(255).required(),              
+      message: Joi.string().min(2).max(255),              
   });
   return schema.validate(comment);
 };
+
+function validateReply(reply){
+    const schema = Joi.object({
+        message: Joi.string().min(2).max(255),              
+    });
+    return schema.validate(comment);
+  };
 
 const Post = mongoose.model("Post", postSchema);
 const Comment = mongoose.model("Comment", commentSchema);
@@ -59,5 +61,8 @@ module.exports = {
     Reply,
     postSchema,
     commentSchema,
-    replySchema
+    replySchema,
+    validatePost,
+    validateComment,
+    validateReply
 };
---
+++ backend/routes/posts.js
@@ -0,0 +1,73 @@
+const { Post, Comment, Reply, postSchema, commentSchema, replySchema, validatePost, validateComment, validateReply } = require("../models/post");
+
+const express = require("express");
+const router = express.Router();
+
+// POST new post
+router.post("/", async (req, res) => {
+    try {
+        const { error } = validatePost(req.body);
+        if (error) return res.status(400).send(error);
+
+        let newPost = await new Post(req.body);
+        await newPost.save();
+
+        return res.status(201).send(newPost);
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
+
+
+//GET all posts
+
+
+//PUT post (Add likes, dislikes, star rating)
+router.put("/:postId/likes", async (req, res) => {
+    try {       
+        let post = await Post.findById(req.params.postId);        
+        if (!post) return res.status(400).send(`Post does not exist!`)        
+        post.likes++;        
+        await post.save();        
+        return res.status(200).send(post);        
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
+
+//PUT Dislikes
+router.put("/:postId/dislikes", async (req, res) => {
+    try {       
+        let post = await Post.findById(req.params.postId);        
+        if (!comment) return res.status(400).send(`Post does not exist!`)        
+        post.dislikes++;        
+        await post.save();        
+        return res.status(200).send(post);        
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
+
+//POST new comment
+router.put("/:postId/comments", async (req, res) => {
+    try {       
+        let post = await Post.findById(req.params.postId);        
+        if (!post) return res.
+            status(400)
+            .send(`Post does not exist!`)        
+        let newComment = await new Comment(req.body);        
+        post.comments.push(newComment);
+        post.save();        
+        return res.status(200).send(post);        
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
+
+//GET comments
+
+
+//PUT comment replies
+
+
+module.exports = router;
