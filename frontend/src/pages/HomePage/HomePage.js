14fc108def2d0896b5c4f64bcf124e1e8e7e62d6
varNum462 <100005058+varNum462@users.noreply.github.com>
2022-04-25 22:21:45 -0400

    Created first draft of displaying Posts, some styling to help visualize layout and content

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
+import MainFeed from "../../components/HomePage/MainFeed/MainFeed"
 
 const HomePage = () => {
   const { user } = useContext(AuthContext);
-  return <h1 className="container">Home Page for {user.name}!</h1>;
+  return (
+  <div className="container">
+    <h1 className="text-left">Home Page for {user.name}</h1>
+    <div className="container d-flex">
+      <MainFeed userId = {user._id} />
+      <div className="w-25 m-2 border border-start">
+       <Friends />
+       <PendingFriends />
+      </div>
+    </div>
+  </div>)
 };
 
 export default HomePage;
