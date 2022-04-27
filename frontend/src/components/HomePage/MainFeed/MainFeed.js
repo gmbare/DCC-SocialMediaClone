f1f38af3a6e1550930e26da1bf487971018a1444
varNum462 <100005058+varNum462@users.noreply.github.com>
2022-04-27 18:16:05 -0400

    Star rating system functional

--- frontend/src/components/HomePage/MainFeed/MainFeed.js
+++ frontend/src/components/HomePage/MainFeed/MainFeed.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import './MainFeed.css';
 import { FaStar } from 'react-icons/fa';
+import Rating from "./Rating";
 
 function MainFeed(props){
     const [Post, setPost] = useState([]);
@@ -18,18 +19,15 @@ function MainFeed(props){
     <div className="w-75 p-0 m-2">        
         <div align="center">
             {Post.map((posts) => {
+               let totalStars = posts.star1 + (posts.star2*2) + (posts.star3*3) + (posts.star4*4) + (posts.star5*5);
+               let numRatings = posts.star1 + posts.star2 + posts.star3 + posts.star4 + posts.star5;
+               let avgStars = totalStars / numRatings;
+               avgStars = parseFloat(avgStars).toFixed(2);
           return (                         
               <div className="postholder p-2 mb-5 border border-warning" key={posts._id}>
-                <div className="stars text-center">Rate this burger:</div>
-                <div className="starholder">
-                  <a className="star"><FaStar /></a>
-                  <a className="star"><FaStar /></a>
-                  <a className="star"><FaStar /></a>
-                  <a className="star"><FaStar /></a>
-                  <a className="star"><FaStar /></a>
-                </div>
-                <img src="../images/burger.jpg" alt={`image-${posts._id}`} />                
-                <div className="message"><p>{posts.message}</p><div className="currentrating">Rating: 5<FaStar className="icon" /></div></div>               
+                <Rating postId={posts._id} getPosts={getPosts} />
+                <img src="../images/burger.jpg" alt={`image-${posts._id}`} />                            
+                <div className="message"><p>{posts.message}</p><div className="currentrating">Rating: {avgStars}<FaStar className="icon" /></div></div>               
               </div>
             )
           })        
