fe8168608193413932f6ca2e2d91718f14a7a7ab
Dustin Barker <barkerd85@yahoo.com>
2022-04-26 18:58:40 -0600

    Added pending friends to FE

--- frontend/src/components/FriendsList/FriendsList.jsx
+++ frontend/src/components/FriendsList/FriendsList.jsx
@@ -5,6 +5,7 @@ import AuthContext from "../../context/AuthContext";
 export default function FriendsList() {
   const { user } = useContext(AuthContext);
   const [friends, setFriends] = useState([]);
+  const [pendingFriends, setPendingFriends] = useState([]);
   const [followed, setFollowed] = useState([]);
 
   const getFriends = async () => {
@@ -23,6 +24,23 @@ export default function FriendsList() {
     getFriends();
   }, []);
 
+  const getPendingFriends = async () => {
+    try {
+      const pendingFriendList = await axios.get(
+        `http://localhost:3008/api/users/${user._id}/pendingFriends`
+      );
+      setPendingFriends(pendingFriendList.data);
+      console.log(pendingFriends);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getFriends();
+    getPendingFriends();
+  }, []);
+
   return (
     <div>
       <h2>
@@ -35,8 +53,20 @@ export default function FriendsList() {
             <li className="list-group-item" key={index}>{friend}</li>)
           })}
         </ul>
+        <div>
+            <h2>
+                Pending Friends
+            </h2>
+            <ul className="list-group">
+          {pendingFriends.map((pendingFriend,index) => {
+            return  (
+            <li className="list-group-item" key={index}>{pendingFriend}</li>)
+          })}
+            </ul>
+        </div>
       </div>
     </div>
+    
   );
 }
 
