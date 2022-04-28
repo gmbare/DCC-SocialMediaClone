8a7c1fa35aec0fcc56b895eb72be3c6b6bc80fc7
Dustin Barker <barkerd85@yahoo.com>
2022-04-28 17:21:04 -0600

    added profile page button route

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,24 +1,30 @@
-import React, { useState, useContext} from 'react';
+import React, { useState, useContext } from "react";
 // import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
-import MainFeed from "../../components/HomePage/MainFeed/MainFeed"
+import MainFeed from "../../components/HomePage/MainFeed/MainFeed";
 import FriendsList from "../../components/FriendsList/FriendsList";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   const { user } = useContext(AuthContext);
-  const [mFriends, setMFriends] = useState()
+  const [mFriends, setMFriends] = useState();
   // const [mFriends, setMFriends] = useState({user:'',post:['']})
 
-    return (
-  <div className="container">
-    <h1 className="text-left">Home Page for {user.name}</h1>
-    <div className="container d-flex">
-      <MainFeed userId = {user._id} mFriends={mFriends} />
-      <div className="w-25 m-2 border border-start">
-       <FriendsList userId={user._id} setMFriends={setMFriends}/>
+  return (
+    <div className="container">
+      <h1 className="text-left">Home Page for {user.name}</h1>
+      <div className="container d-flex">
+        <MainFeed userId={user._id} mFriends={mFriends} />
+        <div className="w-25 m-2 border border-start">
+          <div></div>
+          <FriendsList userId={user._id} setMFriends={setMFriends} />
+        </div>
+        <Link type="button" to="/profile">
+          Edit Profile
+        </Link>
       </div>
     </div>
-  </div>)
+  );
 };
 
 export default HomePage;
--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -1,24 +1,22 @@
-import React, { useState, useContext} from 'react';
+import React, { useState, useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import FriendsList from "../../components/FriendsList/FriendsList";
-
-
+import { Link } from "react-router-dom";
 
 const ProfilePage = () => {
   const { user } = useContext(AuthContext);
-  const [mFriends, setMFriends] = useState()
-  
+  const [mFriends, setMFriends] = useState();
 
-    return (
-  <div className="container">
-    <h1 className="text-left">Home Page for {user.name}</h1>
-    <div className="container d-flex">
-      
-      <div className="w-25 m-2 border border-start">
-       <FriendsList userId={user._id} setMFriends={setMFriends}/>
+  return (
+    <div className="container">
+      <h1 className="text-left">Home Page for {user.name}</h1>
+      <div className="container d-flex">
+        <div className="w-25 m-2 border border-start">
+          <FriendsList userId={user._id} setMFriends={setMFriends} />
+        </div>
       </div>
     </div>
-  </div>)
+  );
 };
 
 export default ProfilePage;
