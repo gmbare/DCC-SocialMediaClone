28c16d1539620b758edcf996364108bfec58e810
Dustin Barker <barkerd85@yahoo.com>
2022-04-28 14:21:38 -0600

    test

--- frontend/src/components/FriendsList/FriendsList.jsx
+++ frontend/src/components/FriendsList/FriendsList.jsx
@@ -2,15 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import AuthContext from "../../context/AuthContext";
 
-<<<<<<< HEAD
-
-
-
 const FriendsList = (props) => {
-
-=======
-const FriendsList = (props) => {
->>>>>>> e0264e52048101d887a7c67aa300c9886dd62cdf
   const { user } = useContext(AuthContext);
   const [friends, setFriends] = useState([]);
   const [friendsNames, setFriendsNames] = useState([]);
@@ -55,16 +47,6 @@ const FriendsList = (props) => {
     }
   };
 
-<<<<<<< HEAD
-
-const acceptFriend = async (e,friendId) => {
-  const acceptedFriend = await axios.put(`/${user.Id}/friend/${friendId}`)
-}
-
-  useEffect(() => {
-    getFriends();
-    getPendingFriends();
-=======
   const acceptFriend = async (e,index) => {
     console.log(`http://localhost:3008/api/users/${user._id}/friend/${pendingFriends[index]}`)
     const acceptedFriend = await axios.put(`http://localhost:3008/api/users/${user._id}/friend/${pendingFriends[index]}`)
@@ -75,7 +57,6 @@ const acceptFriend = async (e,friendId) => {
   useEffect(async () => {
     await getFriends();
     await getPendingFriends();
->>>>>>> e0264e52048101d887a7c67aa300c9886dd62cdf
   }, []);
 
     
@@ -85,19 +66,17 @@ const acceptFriend = async (e,friendId) => {
   
 
   return (
-    <div><h2>
+    <div>
+      <h2>
                 Pending Friends 
             </h2>
             <ul  className="list-group">
-            {/* <ul onSubmit={addFriendButton} className="list-group"> */}
+           
           {pendingFriends.map((pendingFriend,index) => {
             return  (
               
             <li className="list-group-item" key={index}>{pendingFriend} 
-            {/* <form onSubmit={(e) => props.handleAddFriend(e, props.index)}>
-              <input type="hidden" value={pendingFriend} />
-              <input type="submit" value="Add" />
-            </form>   */}
+           
             </li>
             )
           })}
@@ -105,50 +84,21 @@ const acceptFriend = async (e,friendId) => {
       <h2>
         Friends 
       </h2>
-      <div>
+      
         
         <ul className="list-group">
           {friendsNames.map((friend,index) => {
             return  (
-<<<<<<< HEAD
-            < li className="list-group-item" key={index}>{friend} <button>Add</button> <button>Deny</button>  </li>
-           
-            )
-=======
             <li className="list-group-item" key={index}>{friend}</li>)
           })}
         </ul>
-        <div>
-            <h2>
-                Pending Friends
-            </h2>
-            <ul className="list-group">
-          {pendingFriendsNames.map((pendingFriend,index) => {
-            return  (
-            <li className="list-group-item" key={index}>{pendingFriend}
-            {/* <button onClick={((e) => {acceptFriend(e, index)})}>A</button> */}
-            {/* <button onClick={((e) => {acceptFriend(e, index)})}>X</button> */}
-            </li>)
->>>>>>> e0264e52048101d887a7c67aa300c9886dd62cdf
-          })}
+        
+          
        
-            </ul>
         </div>
-      </div>
+      
     
     
   );
-<<<<<<< HEAD
 }
-
-
- 
 export default FriendsList;
-
-
-
-  
-=======
-        };
-export default FriendsList;
->>>>>>> e0264e52048101d887a7c67aa300c9886dd62cdf
