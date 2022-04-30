9d5d977ef43fa318a3064ba523eb4b90ced3a3c6
varNum462 <100005058+varNum462@users.noreply.github.com>
2022-04-29 23:03:19 -0400

    CSS for FriendList, Search, Profile page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -15,10 +15,12 @@ const HomePage = () => {
       <h1 className="text-left">Home Page for {user.name}</h1>
       <div className="container d-flex">
         <MainFeed userId={user._id} mFriends={mFriends} />
-        <div className="m-5 border border-start pos-sticky">
+        <div className="m-5 ps-3 border-start border-dark pos-sticky">
           <Link type="button" to="/profile" className="editProfile "> 
-          <img src="../images/burger.jpg" alt={`image-${user._id}`} />   
-          <p className="editProfile-text">{user.name}</p></Link>       
+            <img src="../images/burger.jpg" className="sm-avatar m-0" alt={`image-${user._id}`} />   
+            <p className="editProfile-text m-0 p-0 text-center ps-2">{user.name}</p>
+            <div className="text-center w-100 m-0 p-0"><small>view profile</small></div>
+          </Link>       
           <FriendsList userId={user._id} setMFriends={setMFriends} />
         </div>
       </div>
--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -3,6 +3,12 @@ import AuthContext from "../../context/AuthContext";
 import FriendsList from "../../components/FriendsList/FriendsList";
 import axios from "axios";
 import AddAbouts from "../../components/AboutMe/AboutMe";
+import { Link } from "react-router-dom";
+import { FaHome } from 'react-icons/fa';
+import { FaEdit } from 'react-icons/fa';
+import { RiImageEditFill } from 'react-icons/ri';
+import "./ProfilePage.css"
+import "./ProfilePage.css"
 
 const ProfilePage = () => {
   const { user } = useContext(AuthContext);
@@ -13,13 +19,31 @@ const ProfilePage = () => {
   return (
     <div className="container">
       <h1 className="text-left">Home Page for {user.name}</h1>
-      <div className="container d-flex">
-        <FriendsList userId={user._id} setMFriends={setMFriends} />
-        <div className="w-25 m-2 border border-start">
+      <div className="container d-flex">        
+        <div className="w-75 m-2 border border-start">
           <div className="container">
+            <div className="d-flex">
+              <div className="lg-avatar">
+                <img src="./images/burger.jpg" alt="User Name here" />
+                <div className="editIcon"><RiImageEditFill /></div>
+              </div>
+              <div className="mt-2 ms-3 editInfo">
+                <div className="p-info-icon"><FaEdit /></div>
+                <div className="d-flex">
+                  <div className="label">Name: </div><div className="profileinfo">{/*NAME GOES HERE*/}</div>
+                </div>
+                <div className="d-flex">
+                  <div className="label">Edit: </div><div className="profileinfo">{/*EMAIL GOES HERE*/}</div>
+                </div>
+              </div>
+            </div>
             <AddAbouts userId={user._id}  />
           </div>
-        </div>
+        </div> 
+        <div> 
+        <Link type="button" className="home-btn" to="/"><FaHome /></Link>    
+          <FriendsList userId={user._id} setMFriends={setMFriends} />
+        </div> 
       </div>
     </div>
   );
