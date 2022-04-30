8c0ced602205a32cdedddbcc72ced46898342bac
Dustin Barker <barkerd85@yahoo.com>
2022-04-29 21:05:06 -0600

    added profile page about me save

--- frontend/src/components/AboutMe/AboutMe.js
+++ frontend/src/components/AboutMe/AboutMe.js
@@ -1,42 +1,75 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 
+
 function AddAbouts(props) {
-  const [abouts, setAbouts] = useState("");
-  async function handleSubmit(event) {
-    event.preventDefault();
-    let strAboutMe = document.getElementById("commentField").value;
-   
-    console.log(strAboutMe);
-    console.log(props.userId);
-    await axios.post(`http://localhost:3008/api/abouts/${props.userId}`, {
-      text: strAboutMe,
-      
-    });
+  const [about, setAbouts] = useState("");
+  const { user } = useContext(AuthContext);
+  
+  
+
+
+  async function getAboutMe() {
+    let userAbout = await axios.get(`http://localhost:3008/api/abouts/${user._id}`);
+    setAbouts(userAbout.data.about.text);
   }
 
-  useEffect(() => {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let newAboutMe = {
+      aboutMe: about,
+    };
+    await axios.put(
+      `http://localhost:3008/api/abouts/${user._id}`,
+      newAboutMe
+      
+    );
+    
+  }
+
+
+
+  // async function handleSubmit(event) {
+  //   event.preventDefault();
+  //   let strAboutMe = { aboutMe: about,
+  //   };
+   
+  //   console.log(strAboutMe);
+  //   console.log(props.userId);
+  //   await axios.post(`http://localhost:3008/api/abouts/${props.userId}`, {
+  //     text: strAboutMe,
+      
+  //   });
+  // }
+ useEffect(() => {
+    getAboutMe();
     handleSubmit();
   }, []);
 
   return (
     <div id="addComment">
-      <form id="commentform">
+      <form id="AboutMe" onSubmit={(event) => handleSubmit(event)}>
         <div className="text-start">
           <label>
             <h4 className="text-dark">About me:</h4>
           </label>
+          
           <input
             className="form-control m-2 mb-3 border border-success"
             type="text"
             name="comment"
             id="commentField"
-            defaultValue={"About me..."}
+            value={about}
+            onChange={(e) => setAbouts(e.target.value)}
+            
+          
           ></input>
-        </div>
+        
         <div>
           <input
+          
             className="btn btn-success"
             type="submit"
             value="Submit"
@@ -44,7 +77,7 @@ function AddAbouts(props) {
               handleSubmit(event);
               alert("About me saved!");
             }}
-          />
+          /></div>
         </div>
       </form>
     </div>
@@ -52,3 +85,8 @@ function AddAbouts(props) {
 }
 
 export default AddAbouts;
+
+    
+    
+
+ 
