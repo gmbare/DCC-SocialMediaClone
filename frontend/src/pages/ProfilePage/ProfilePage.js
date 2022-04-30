7dfca4c0ae1afc0b38942cbb0979c2ab7dd51d39
Dustin Barker <barkerd85@yahoo.com>
2022-04-30 17:49:42 -0600

    adding video bg

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -10,7 +10,7 @@ import "./ProfilePage.css"
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import FormData from 'form-data'
-// import burgerbackground from './assets/burgerbackground.mp4'
+import burgerbackground from '../Assets/burgerbackground.mp4'
 
 const ProfilePage = () => {
   const { user } = useContext(AuthContext);
@@ -64,8 +64,8 @@ const ProfilePage = () => {
 
 
   return (
-    <div className="container">
-      {/* <video src={burgerbackground} /> */}
+    <div className="container-fluid">
+      <video className="bgvideo" src={burgerbackground}  autoPlay loop muted />
       <h1 className="text-left">Home Page for {user.name}</h1>
       <div className="container d-flex">
         <div className="w-75 m-2 border border-start">
